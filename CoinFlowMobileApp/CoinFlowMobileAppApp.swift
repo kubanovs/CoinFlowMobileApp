cbb24a8f2623fc8ad79fc4b539aba70399d594fe
kubanovs <svkubanov@edu.hse.ru>
2025-02-02 20:40:00 +0300

    init project

---
+++ CoinFlowMobileApp/CoinFlowMobileAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoinFlowMobileAppApp.swift
+//  CoinFlowMobileApp
+//
+//  Created by Savr Kubanov on 02.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct CoinFlowMobileAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
